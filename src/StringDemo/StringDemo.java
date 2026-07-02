9b56518af935a49fdfd86098fd15ee8cdf317eba
Rajnish rai <rajnishrai277123@gmail.com>
2026-07-02 22:12:20 +0530

    final

--- src/StringDemo/StringDemo.java
+++ src/StringDemo/StringDemo.java
@@ -4,7 +4,9 @@ public class StringDemo {
     public static void main(String[] args) {
         String s1=new String("rajnish");
         String s2="rajnish";
+        String s3="raj";
         System.out.println(s1.hashCode());
         System.out.println(s2.hashCode());
+        System.out.println(s3.hashCode());
     }
 }
